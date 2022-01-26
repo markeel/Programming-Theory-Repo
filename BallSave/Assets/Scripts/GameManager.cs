8d44ca1dd86c41be23d81ec88aca37f0265da2f1
markeel <40717746+markeel@users.noreply.github.com>
2022-01-26 16:12:39 -0700

    Day 1 Updates
    Created main scene with algorithmically defined tiles with holes in view.

--- BallSave/Assets/Scripts/GameManager.cs
+++ BallSave/Assets/Scripts/GameManager.cs
@@ -4,10 +4,64 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject tilePrefab;
+    [SerializeField]
+    private GameObject tileWithHolePrefab;
+
+    [SerializeField]
+    private Material aMat;
+    [SerializeField]
+    private Material bMat;
+
+    [SerializeField]
+    private float spacingFactor = 1.1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 pos = new Vector3(0, 0, 0);
+        GameObject tile = Instantiate(tilePrefab, pos, tilePrefab.transform.rotation);
+        tile.GetComponent<Renderer>().material = bMat;
+        Vector3 size = tile.GetComponentInChildren<MeshRenderer>().bounds.size;
+        float xoffset = ((size.x * spacingFactor * 3) + size.x) / 2;
+        float xstart = xoffset;
+        float zoffset = ((size.z * spacingFactor * 4) + size.x) / 2;
+        float zstart = -zoffset;
+        pos.x = xstart;
+        pos.z = zstart;
+        pos.x -= size.x * spacingFactor;
+        tile.transform.position = new Vector3(xstart, tile.transform.position.y, zstart);
+        int row = 0;
+        int col = 1;
+        HashSet<int> holes = new HashSet<int>();
+        for (int hole = 0; hole < 4; hole++)
+            holes.Add(Random.Range(5, 5 * 5));
+
+        while (row < 6)
+        {
+            while (col < 5)
+            {
+                bool isHole = holes.Contains(row * 5 + col);
+
+                if (isHole)
+                    tile = Instantiate(tileWithHolePrefab, pos, tileWithHolePrefab.transform.rotation);
+                else
+                    tile = Instantiate(tilePrefab, pos, tilePrefab.transform.rotation);
+
+                if ((((row % 2) == 0) && (col % 2 == 1)) || (((row % 2) == 1) && (col % 2 == 0)))
+                    tile.GetComponent<Renderer>().material = aMat;
+                else
+                    tile.GetComponent<Renderer>().material = bMat;
+
+                col += 1;
+                pos.x -= size.x * spacingFactor;
+            }
+            pos.x = xstart;
+            col = 0;
+            pos.z += size.z * spacingFactor;
+            row += 1;
+        }
     }
 
     // Update is called once per frame
---
+++ timeline.md
@@ -0,0 +1,24 @@
+# Timeline
+
+## Day 1
+   * Grid appears (random holes) using prefabs
+       * Done
+## Day 2
+   * Mouse will highlight tiles with circle (non-hole) or "X" (hole)
+## Day 3
+   * Pieces appear on tiles
+   * Pieces can be selected (piece has diamond appear above it)
+## Day 4
+   * Mouse highlights tiles based on selected piece's rules
+   * Selecting tile with a circle will move the piece (slight arc between the 2 squares over a short period 1-2 seconds)
+## Day 5
+   * Launcher appears on top row at start
+   * Launcher disappears and missle moves 2 squares (slight arc between the squares) after a player move
+   * Collision of missile with pieces (cube, cylinder, sphere)
+   * Dissappearance of missle resets launcher
+## Day 6
+   * Game Lost message
+   * Game Won message
+   * Game Start menu button
+
+
